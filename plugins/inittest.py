32014102a7424a9b6d92080c0fc0e0fc34916e10
Werameli <private@microsoft.com>
2024-08-13 22:04:25 +0300

    pre-Beta 1.5 (The most lazy update yet)

--- plugins/inittest.py
+++ plugins/inittest.py
@@ -1,31 +1,31 @@
 import lib.globalplaceholders as gph
 from lib.decorator import shell_command
 
-class PlugInfo:
-    def __init__(self, shell_instance):
+class PlugInfo: # DO NOT REMOVE
+    def __init__(self, shell_instance): # DO NOT REMOVE
         self.shell_instance = shell_instance
 
     @staticmethod
-    def SNKInit():
+    def SNKInit(): # NO NOT REMOVE
         return {
-            "name": "PluginExample",
-            "vendor": "Werameli",
-            "version": gph.version, # add your version by using ""
-            "description": "Example plugin with simple functions and init function"
+            "name": "PluginExample", # Add your plugin name here
+            "vendor": "Werameli", # Add yourself here :)
+            "version": gph.version, # Add your version by using ""
+            "description": "Example plugin with simple functions" # Add your plugin's description here
         }
 
-    def init(self): # Unnecessary function
+    def init(self): # DO NOT REMOVE
         print(f"{self.SNKInit()['name']} initialized.")
 
 
     @staticmethod
-    def cleanup(cls, shell_instance): # Unnecessary function
+    def cleanup(cls, shell_instance): # DO NOT REMOVE
         for command in cls.commands_to_remove:
             if hasattr(shell_instance.__class__, command):
                 delattr(shell_instance.__class__, command)
                 print(f"Removed command: {command}")
 
-class ShellIntegrations:
+class ShellIntegrations: # Add your command which you wanted to type in SNK's Shell here
     @shell_command
     def do_test(self, arg):
         print("hi!")
