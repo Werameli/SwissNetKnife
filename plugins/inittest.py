a6e836ef06a22175032ade0cdd3c06626e312600
Werameli <private@microsoft.com>
2024-08-13 23:58:12 +0300

    pre-Beta 1.6.1

--- plugins/inittest.py
+++ plugins/inittest.py
@@ -17,16 +17,6 @@ class PlugInfo: # DO NOT REMOVE
     def init(self): # DO NOT REMOVE
         pass
 
-    def cleanup(self): # DO NOT REMOVE
-        shell_integrations_class = getattr(self, 'ShellIntegrations', None)
-        if shell_integrations_class:
-            commands_to_remove = shell_integrations_class.commands_to_remove
-            for command_name in commands_to_remove:
-                if hasattr(self.shell_instance.__class__, command_name):
-                    delattr(self.shell_instance.__class__, command_name)
-                    print(f"Removed command: {command_name}")
-        print(f"{self.SNKInit()['name']} cleaned up.")
-
 class ShellIntegrations: # Add your command which you wanted to type in SNK's Shell here
     commands_to_remove = []
     @shell_command
